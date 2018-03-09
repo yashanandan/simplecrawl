e6fe366d7b1ea37ba8d78576ab4beac6201311f0
Yashwanth Anandan <yashanandan@gmail.com>
2018-03-09 22:36:00 +0530

    final changes...hopefully

--- Crawl.py
+++ Crawl.py
@@ -1,20 +1,33 @@
+"""Simple Crawler
+
+Usage:
+    Crawl.py -s <startpage> -m <maxpages>
+
+
+
+Options:
+  -h, --help     Show this screen.
+  -s, --startpage <startpage>   URL to begin Crawl, default is http://python.org
+  -m, --maxpages <maxpages>     Maximum pages to crawl, default is 100
+
+"""
+
 import collections
-import string
 
 from timeit import default_timer
 from urllib.parse import urldefrag, urljoin, urlparse
 
 import bs4
+from docopt import docopt
 import requests
 
 #------------------------------------------------------------------------------
-def crawler(startpage, maxpages=100, singledomain=True):
-
+def crawler(startpage='http://python.org', maxpages=100):
 
     pagequeue = collections.deque() # queue of pages to be crawled
     pagequeue.append(startpage)
     crawled = [] # list of pages already crawled
-    domain = urlparse(startpage).netloc if singledomain else None
+    domain = urlparse(startpage).netloc
 
     pages = 0 # number of pages succesfully crawled so far
     failed = 0 # number of links that couldn't be crawled
@@ -22,6 +35,8 @@ def crawler(startpage, maxpages=100, singledomain=True):
     sess = requests.session() # initialize the session
     while pages < maxpages and pagequeue:
         url = pagequeue.popleft() # get next page to crawl (FIFO queue)
+        if ( url.startswith("//") ):
+            url = "http:" + url
 
         # read the page
         try:
@@ -41,7 +56,7 @@ def crawler(startpage, maxpages=100, singledomain=True):
         # process the page
         crawled.append(url)
         pages += 1
-        if pagehandler(url, response, soup):
+        if pagehandler(url, response):
             # get the links from this page and add them to the crawler queue
             links = getlinks(url, domain, soup)
             for link in links:
@@ -52,7 +67,13 @@ def crawler(startpage, maxpages=100, singledomain=True):
 
 #------------------------------------------------------------------------------
 def getlinks(pageurl, domain, soup):
-
+    '''
+    Gets the links within the page
+    :param pageurl:
+    :param domain:
+    :param soup:
+    :return:
+    '''
     # get target URLs for all links on the page
     links = [a.attrs.get('href') for a in soup.select('a[href]')]
 
@@ -73,14 +94,21 @@ def getlinks(pageurl, domain, soup):
     return links
 
 #------------------------------------------------------------------------------
-def pagehandler(pageurl, pageresponse, soup=None):
+def pagehandler(pageurl, pageresponse):
 
     print('Crawling:' + pageurl + ' ({0} bytes)'.format(len(pageresponse.text)))
     return True
 
 #------------------------------------------------------------------------------
 def samedomain(netloc1, netloc2):
-
+    '''
+    This method makes sure we stay within the start page's domain by comparing the url.
+    google.com == drive.google.com== mail.google.com == True
+    this functiion ensures we stay within google.com and any sub domain is irrelevent
+    :param netloc1:
+    :param netloc2:
+    :return:
+    '''
     domain1 = netloc1.lower()
     if '.' in domain1:
         domain1 = domain1.split('.')[-2] + '.' + domain1.split('.')[-1]
@@ -93,7 +121,13 @@ def samedomain(netloc1, netloc2):
 
 #------------------------------------------------------------------------------
 def url_in_list(url, listobj):
-
+    '''
+        since both the https and https links have the same content it would be enough if we crawl either so this function
+        avoid re-crawling of a page
+    :param url:
+    :param listobj:
+    :return:
+    '''
     http_version = url.replace('https://', 'http://')
     https_version = url.replace('http://', 'https://')
     return (http_version in listobj) or (https_version in listobj)
@@ -101,7 +135,8 @@ def url_in_list(url, listobj):
 #------------------------------------------------------------------------------
 
 if __name__ == "__main__":
+    arguments = docopt(__doc__)
     START = default_timer()
-    crawler('http://python.org', maxpages=100, singledomain=True)
+    crawler(arguments['--startpage'], maxpages=int(arguments['--maxpages']))
     END = default_timer()
     print('Elapsed time (seconds) = ' + str(END-START))
